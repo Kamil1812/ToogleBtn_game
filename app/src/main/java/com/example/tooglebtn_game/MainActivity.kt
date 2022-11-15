a7b5c5bc4f4d53dc593e18df73ed2189f6181f54
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-11-15 11:17:18 +0100

    button clear and checking length

--- app/src/main/java/com/example/tooglebtn_game/MainActivity.kt
+++ app/src/main/java/com/example/tooglebtn_game/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val numbers: Array<Int> = arrayOf(4, 7, 3, 9, 1, 8, 5, 2, 6)
-        val numbers_odp: Array<Int> = arrayOf()
         val score = findViewById<TextView>(R.id.tV_score)
         val output = findViewById<TextView>(R.id.tV_result)
 
@@ -27,41 +26,62 @@ class MainActivity : AppCompatActivity() {
         val toggle8 = findViewById<ToggleButton>(R.id.tB8)
         val toggle9 = findViewById<ToggleButton>(R.id.tB9)
 
-        findViewById<ToggleButton>(R.id.tB1).setOnClickListener{
-            output.text = output.text.toString() + "1"
-        }
+            findViewById<Button>(R.id.btn_clear).setOnClickListener {
+                output.text = ""
+            }
 
-        findViewById<ToggleButton>(R.id.tB2).setOnClickListener{
-            output.text = output.text.toString() + "2"
-        }
+            findViewById<ToggleButton>(R.id.tB1).setOnClickListener {
+                if (output.text.contains("1") == false && output.text.length < 9) {
+                    output.text = output.text.toString() + "1"
+                }
+            }
 
-        findViewById<ToggleButton>(R.id.tB3).setOnClickListener{
-            output.text = output.text.toString() + "3"
-        }
+            findViewById<ToggleButton>(R.id.tB2).setOnClickListener {
+                if (output.text.contains("2") == false && output.text.length < 9) {
+                    output.text = output.text.toString() + "2"
+                }
+            }
 
-        findViewById<ToggleButton>(R.id.tB4).setOnClickListener{
-            output.text = output.text.toString() + "4"
-        }
+            findViewById<ToggleButton>(R.id.tB3).setOnClickListener {
+                if (output.text.contains("3") == false && output.text.length < 9) {
+                    output.text = output.text.toString() + "3"
+                }
+            }
 
-        findViewById<ToggleButton>(R.id.tB5).setOnClickListener{
-            output.text = output.text.toString() + "5"
-        }
+            findViewById<ToggleButton>(R.id.tB4).setOnClickListener {
+                if (output.text.contains("4") == false && output.text.length < 9) {
+                    output.text = output.text.toString() + "4"
+                }
+            }
 
-        findViewById<ToggleButton>(R.id.tB6).setOnClickListener{
-            output.text = output.text.toString() + "6"
-        }
+            findViewById<ToggleButton>(R.id.tB5).setOnClickListener {
+                if (output.text.contains("5") == false && output.text.length < 9) {
+                    output.text = output.text.toString() + "5"
+                }
+            }
 
-        findViewById<ToggleButton>(R.id.tB7).setOnClickListener{
-            output.text = output.text.toString() + "7"
-        }
+            findViewById<ToggleButton>(R.id.tB6).setOnClickListener {
+                if (output.text.contains("6") == false && output.text.length < 9) {
+                    output.text = output.text.toString() + "6"
+                }
+            }
 
-        findViewById<ToggleButton>(R.id.tB8).setOnClickListener{
-            output.text = output.text.toString() + "8"
-        }
+            findViewById<ToggleButton>(R.id.tB7).setOnClickListener {
+                if (output.text.contains("7") == false && output.text.length < 9) {
+                    output.text = output.text.toString() + "7"
+                }
+            }
 
-        findViewById<ToggleButton>(R.id.tB9).setOnClickListener{
-            output.text = output.text.toString() + "9"
-        }
+            findViewById<ToggleButton>(R.id.tB8).setOnClickListener {
+                if (output.text.contains("8") == false && output.text.length < 9) {
+                    output.text = output.text.toString() + "8"
+                }
+            }
 
+            findViewById<ToggleButton>(R.id.tB9).setOnClickListener {
+                if (output.text.contains("9") == false && output.text.length < 9) {
+                    output.text = output.text.toString() + "9"
+                }
+            }
     }
 }
