37d1c328c45e3ff4e2c2c8ac3cae4fbaf81d394c
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-11-15 10:29:37 +0100

    implementing toggle buttons, text views, button and adding to text view text

--- app/src/main/java/com/example/tooglebtn_game/MainActivity.kt
+++ app/src/main/java/com/example/tooglebtn_game/MainActivity.kt
@@ -11,9 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val numbers : Array<Int> = arrayOf(4,7,3,9,1,8,5,2,6)
-        val numbers_odp : Array<Int> = arrayOf()
-        val output = findViewById<TextView>(R.id.tV_score)
+        val numbers: Array<Int> = arrayOf(4, 7, 3, 9, 1, 8, 5, 2, 6)
+        val numbers_odp: Array<Int> = arrayOf()
+        val score = findViewById<TextView>(R.id.tV_score)
+        val output = findViewById<TextView>(R.id.tV_result)
+
 
         val toggle1 = findViewById<ToggleButton>(R.id.tB1)
         val toggle2 = findViewById<ToggleButton>(R.id.tB2)
@@ -24,5 +26,41 @@ class MainActivity : AppCompatActivity() {
         val toggle7 = findViewById<ToggleButton>(R.id.tB7)
         val toggle8 = findViewById<ToggleButton>(R.id.tB8)
         val toggle9 = findViewById<ToggleButton>(R.id.tB9)
+
+        findViewById<ToggleButton>(R.id.tB1).setOnClickListener{
+            output.text = output.text.toString() + "1"
+        }
+
+        findViewById<ToggleButton>(R.id.tB2).setOnClickListener{
+            output.text = output.text.toString() + "2"
+        }
+
+        findViewById<ToggleButton>(R.id.tB3).setOnClickListener{
+            output.text = output.text.toString() + "3"
+        }
+
+        findViewById<ToggleButton>(R.id.tB4).setOnClickListener{
+            output.text = output.text.toString() + "4"
+        }
+
+        findViewById<ToggleButton>(R.id.tB5).setOnClickListener{
+            output.text = output.text.toString() + "5"
+        }
+
+        findViewById<ToggleButton>(R.id.tB6).setOnClickListener{
+            output.text = output.text.toString() + "6"
+        }
+
+        findViewById<ToggleButton>(R.id.tB7).setOnClickListener{
+            output.text = output.text.toString() + "7"
+        }
+
+        findViewById<ToggleButton>(R.id.tB8).setOnClickListener{
+            output.text = output.text.toString() + "8"
+        }
+
+        findViewById<ToggleButton>(R.id.tB9).setOnClickListener{
+            output.text = output.text.toString() + "9"
+        }
     }
 }
